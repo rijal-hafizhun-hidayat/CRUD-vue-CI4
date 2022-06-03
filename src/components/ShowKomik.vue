976ba6e90d5ae69f3b881624a6d8049f1420c317
unknown <rijal.1344@gmail.com>
2022-06-03 14:24:29 +0700

    add loading spinner

--- src/components/ShowKomik.vue
+++ src/components/ShowKomik.vue
@@ -1,8 +1,16 @@
 <template> 
     <div class="container mt-5">
+        <loading 
+            v-model:active="isLoading"
+            :can-cancel="true"
+            :is-full-page="fullPage"/>
         <div class="row justify-content-center">
             <div class="col-sm-8">
-                <router-link :to="{name: 'komik.create'}" class="btn btn-primary">Add</router-link>
+                <div class="d-flex justify-content-between">
+                    <router-link :to="{name: 'komik.create'}" class="btn btn-primary">Add</router-link>
+                    <input type="text" class="form-control" v-model="search" placeholder="pencarian">
+                </div>
+                
                 <!-- <button class="btn btn-primary"> Add</button> -->
                 <div class="card mt-5">
                     <div class="card-header">
@@ -40,32 +48,55 @@
 
 <script>
 import axios from "axios";
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 export default {
     name: "Show",
     data(){
         return{
-            comic: []
+            comic: [],
+            search: null,
+            isLoading: false,
+            fullPage: true
         }
+    },
+    components: {
+        Loading
     },
     created(){
         this.getComic();
+        this.filter();
     },
     methods:{
         async getComic(){
             try {
+                this.isLoading = true;
                 const response = await axios.get('http://localhost:8080/komik');
                 this.comic = response.data;
+                setTimeout(() => {
+                    this.isLoading = false
+                }, 1000)
             } catch (error) {
                 console.log(error);
             }
         },
         async deleteComic(id){
             try {
+                this.isLoading = true;
                 await axios.delete(`http://localhost:8080/komik/${id}`);
                 this.getComic();
+                setTimeout(() =>{
+                    this.isLoading = false
+                }, 1000)
             } catch (error) {
                 console.log(error);
             }
+        },
+        filter(){
+            if(this.search){
+
+            }
+
         }
     },
 };
